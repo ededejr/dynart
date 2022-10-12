e5d7eee3df8d5426036f3861d869a76f40731cd2
Edede Oiwoh <eoiwoh@gmail.com>
2022-10-11 21:40:47 -0400

    model route for targetted pages and seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  const { m } = router.query || {};
-  const imageRoute = m && typeof m === 'string' && verifyModelParam(m) ? `/api/img/s/${m}` : '/api/img/random';
-
 	return (
 		<>
       <Head>
@@ -20,14 +17,14 @@ export default function App({ Component, pageProps, router }: AppProps) {
         <meta name="twitter:site" content={`@${meta.owner.twitter}`} />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={imageRoute} />
+        <meta name="twitter:image" content={meta.image} />
         
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_us" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" itemProp="image" content={imageRoute} />
+        <meta property="og:image" itemProp="image" content={meta.image} />
         <meta property="og:url" content={meta.url} />
       </Head>
       <Component {...pageProps} />
@@ -43,8 +40,4 @@ const meta = {
   owner: {
     twitter: 'ededejr',
   }
-}
-
-function verifyModelParam(model: string) {
-  return ['clock', 'de-logo', 'dynamic-colorful-polylines', 'gradients-and-flat-colors'].includes(model)
 }
---
+++ pages/model/[id].tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Image from 'next/image'
+import { useRouter } from 'next/router';
+import styles from '../../styles.module.css'
+
+export default function Page() {
+  const router = useRouter();
+  const { id } = router?.query || {};
+  const isVerifed = id && typeof id === 'string' && verifyModelParam(id);
+  const imageRoute = isVerifed ? `/api/img/s/${id}` : '/api/img/random';
+
+  return (
+    <>
+      <Head>
+        <title>{isVerifed ? `dynart - ${id}` : 'dynart'}</title>
+        <meta name="twitter:image" content={imageRoute} />
+        <meta property="og:image" itemProp="image" content={imageRoute} />
+      </Head>
+      <main className={styles.container}>
+        <h1>dynart</h1>
+        {isVerifed ? (
+          <p>
+            The following image is generated using the <code>{id}</code> model.
+          </p>
+        ) : null}
+        <Image src={imageRoute} alt="Generated Image" width={1200} height={630} />
+      </main>
+    </>
+  )
+}
+
+function verifyModelParam(model: string) {
+  return ['clock', 'de-logo', 'dynamic-colorful-polylines', 'gradients-and-flat-colors'].includes(model)
+}
