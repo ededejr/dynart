2906c3f3597fd579884cdb700eeacf23c5a78a1c
Edede Oiwoh <eoiwoh@gmail.com>
2022-10-11 21:32:52 -0400

    again with twitter meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,28 +4,31 @@ import Head from 'next/head';
 export default function App({ Component, pageProps, router }: AppProps) {
   const { m } = router.query || {};
   const imageRoute = m && typeof m === 'string' && verifyModelParam(m) ? `/api/img/s/${m}` : '/api/img/random';
+
 	return (
 		<>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-
+        <meta name="googlebot" content="follow, index, noarchive" />
+        <meta name="robots" content="follow, index, noarchive" />
         <link rel="canonical" href={meta.url} />
 
+        <meta name="description" content={meta.description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={`@${meta.owner.twitter}`} />
+        <meta name="twitter:site" content={`@${meta.owner.twitter}`} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={imageRoute} />
+        
         <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_us" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" itemProp="image" content={imageRoute} />
         <meta property="og:url" content={meta.url} />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={`@${meta.owner.twitter}`} />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={imageRoute} />
       </Head>
       <Component {...pageProps} />
 		</>
